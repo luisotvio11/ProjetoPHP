ff0fd96a600d407bdd415a43e43cb8d09d1b307f
luisotaviodev@outlook.com <luisotaviodev@outlook.com>
2021-09-01 23:33:06 -0300

    Criado arquivo para estudo de tipos de Dados no PHP

---
+++ tiposDeDados.php
@@ -0,0 +1,30 @@
+<?php
+
+$nome ="Luis Otávio"; // Variavel do Tipo String
+$sobrenome = "de Oliveira Batista"; // variavel do tipo String
+$site = 'www.google.com'; // variavel do tipo String
+
+$ano = 1996;  // Variavel do Tipo Numérico INteiro
+
+$salario = 5500.99; //Tipo Float
+
+$bloqueado = false; // tipo booleano
+$Ativo = true; // Tipo Booleano Verdadeiro
+
+//////////////////////////////////////////////////////////////////////
+
+$frutas  = array("Abacaxi", "Goiaba", "Morango", "Banana", " Maça"); // Arrays em PHP
+
+
+echo $frutas [0]; 
+echo "<br/>";
+echo $times [3];
+
+$nascimento = new DateTime(); // 
+
+var_dump($nascimento);
+var_dump($times);
+   
+
+
+
---
+++ variaveis.php
@@ -0,0 +1,17 @@
+<?php
+
+    $anoNascimento = 1996;
+    $nomeCompleto = "Luis Otávio de Oliveira Batista"; //Testando Variaveis
+
+
+    echo $anoNascimento; //imprimindo a variavel anodeNascimento
+
+    echo "<br>";
+
+    unset ($anoNascimento)
+
+    echo $nomeCompleto;
+
+
+
+
